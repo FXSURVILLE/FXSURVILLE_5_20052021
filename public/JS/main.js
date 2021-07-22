6e2c5e15055516a743157039a0d8552b6c8b28a4
FXSURVILLE <francois-xavier@expleogroup.com>
2021-07-22 16:03:36 +0200

    search global

--- public/JS/main.js
+++ public/JS/main.js
@@ -28,10 +28,23 @@ fiches();
 initIdResult();
 
 
+// function infoSearch()
+recipes.forEach(recipe=> {
+    let temp=[];
+    temp.push(recipe.name,recipe.description);
+    for (let i = 0; i < recipe.ingredients.length; i++) {
+        temp.push(recipe.ingredients[i].ingredient)        
+    }
+    arrSearch.push(temp);
+})
+console.log(arrSearch)
+
+
 function initIdResult() {
     idResult=recipes.map(recipe => recipe.id);
 };
 console.log(idResult);
+
 
 // listes dropdown
 let listIngredients;
@@ -79,9 +92,6 @@ console.log(arrayIngredients);
 // tableau des appareils
 const arrayAppliance=recipes.map(e=>e.appliance)
 console.log(arrayAppliance);
-// tableau des descriptions
-// const arrayDescription=recipes.map(e=>e.description)
-// console.log(arrayDescription);
 
 
 //création des fiches recette
@@ -109,26 +119,21 @@ function fiches() {
 searchBarId.addEventListener("input", function(e) {
     let searchRegex = /^.{3,}$/g; // \S ou [a-zA-Z]+cara spé?
     let searchText = e.target.value.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-    if (searchRegex.test(searchText) === true) {
+    if (searchRegex.test(searchText)) {
         searchResult=[];
-        recipes.forEach(e => {
-            if (e.name.toLowerCase().includes(searchText)) { // recherche dans titre
-                searchResult.push(e.id)};
-            if (e.description.toLowerCase().includes(searchText)) { // recherche dans description
-                if (!searchResult.includes(e.id)) {
-                    searchResult.push(e.id)};
-                }
-            // if (e.appliance.toLowerCase().includes(searchText)) { // recherche dans appareils
-            //     indices.push(e.id);
-            //     console.log('testok')};
-            e.ingredients.forEach(ing=>{
-                if (ing.ingredient.toLowerCase().includes(searchText)) { // recherche dans titre
-                    if (!searchResult.includes(e.id)) {
-                        searchResult.push(e.id)};
-                    }
-            })
-            console.log(searchResult)            
-        });
+        for (let i = 0; i < arrSearch.length; i++) {
+            console.log(i)  
+            console.log(arrSearch[i]) 
+            console.log(searchText) 
+            for (let j = 0; j < arrSearch[i].length; j++) {
+                let g=arrSearch[i][j].toLowerCase();
+                if (g.includes(searchText)) { // recherche globale
+                    searchResult.push(i+1)
+                };
+                console.log(g)
+            }
+            console.log(searchResult)      
+        }
         searchFusion()
         majDropdown();
     } else {
@@ -373,229 +378,3 @@ function storeTags() {
     }
     console.log(ingTag);    console.log(appTag);    console.log(ustTag);
 }
-
-
-// console.log(document.getElementsByClassName('bloc-recette'));
-// function filtreFiches() {
-//     console.log(indices);
-//     for (i = 0; i < blocRecette.length; i++) {
-//         td = tr[i].getElementsByTagName("td")[0];
-//         if (td) {
-//             txtValue = td.textContent || td.innerText;
-//             if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//                 tr[i].style.display = "";
-//             } else {
-//                 tr[i].style.display = "none";
-//             }
-//         }       
-//     }
-// }
-
-
-// maj tableau suivant recherches
-// function idResults() {
-//     if (id2.length!=0) {   
-//         let a=idResult.filter(x => id2.includes(x));
-//         idResult=a;
-//         console.log(idResult);
-//     }else{
-//         alert ("vide");
-//     }
-//     if (id3.length!=0) {   
-//         let a=idResult.filter(x => id3.includes(x));
-//         idResult=a;
-//         console.log(idResult);
-//     }else{
-//         alert ("vide");
-//     }
-//     if (id4.length!=0) {   
-//         let a=idResult.filter(x => id4.includes(x));
-//         console.log(id4);
-//         idResult=a;
-//         console.log(idResult);
-//     }else{
-//         alert ("vide");
-//     }
-//     if (id5.length!=0) {   
-//         let a=idResult.filter(x => id5.includes(x));
-//         idResult=a;
-//         console.log(idResult);
-//     }else{
-//         alert ("vide");
-//     }
-// }
-
-
-// foreach if regex= name let id 
-
-
-// function liste() {
-// let liste = "";
-// for(var i = 0; i < length; i++){
-//     liste += recipes.ingredients[i].ingredient+"\n";
-//     console.log(liste);
-// }
-// return liste;
-// };
-
-
-// 0 recherche dans tout
-
-// 1 recherche dans classes des fiches
-
-/*
-recherche
-concat ing+app+ust+recet
-if rech+tag false = all id
-forEach id concat
-if rech dans concat =true add id dans Set else remove id
-if tag dans array =true add id dans Set else remove id
-*/
-
-// 2 recherche dans tableau
-/*
-recherche
-foreach id rech if recet else ing else app else ust
-*/
-
-// let recupID=nomarray.map(e=>e.id) ?
-
-// recupIngredients.forEach(e => e.forEach(i => console.log("<strong>"+i.ingredient+":</strong>"+i.quantity+i.unit)));
-
-// // //surveille la recherche
-// const rechercheTexte = document.getElementById('recherche__texte');
-// rechercheTexte.addEventListener('input',function recherche(){
-//     if ()
-// });
-
-// function recherche() {}
-
-// const recipesName = recipes.map(recipe => recipe.name); 
-
-// afficher les ingrédients
-// let listeIngredients = ingredients.map(e => ingredients + ": " quantity + unit);
-
-// // filtrer (ingredientsFiltres= tous les éléments du tableau ingrédients moins l'elementdufiltre )
-// let ingredientsFiltres = ingredients.filter(function(ingredient) {
-//     if(ingredient != "elementdufiltre"){
-//         return true; // ou: return ingrédient;
-//     }
-// })
-// = si un seul parametre
-// let ingredientsFiltres = ingredients.filter(function(ingredient) {
-//     if(ingredient != "elementdufiltre"){
-//         return true; // ou: return ingrédient;
-//     }
-// })
-
-
-// class FicheRecetteElement extends HTMLElement {
-//     constructor(){
-//         super();
-//     }
-//     connectedCallback(){
-//         // let name = recipe.name;
-//         // let ingredients = this(recipe.ingredients);
-//         // let description = this(recipe.description);
-//         this.innerHTML = `
-//         <div class="bloc-recette">
-//             <div class="img">
-//                 <img class="image" src="" alt="" title="">
-//             </div>
-//             <div class="recette">
-//                 <div class="titre">zgzegsdsgsdgsd</div>
-//                 <p class="duree"><i class="far fa-clock"></i> 166 min</p>
-//                 <div class="ingredients">dthf</div>
-//                 <div class="description">shdzfgee eeeeeeeeeeeeee qzegagzahe zhqfqzdssss sssssssssss ssssssssssssss ssssssssss ssssssssssss sssssssssssss sssssssss</div>
-//             </div>
-//         </div>
-//         `;
-//     }
-// }
-// window.customElements.define('fiche-recette',FicheRecetteElement);
-
-
-// let searchAll=[];
-// searchAll += JSON.stringify(recipes[0].ingredients);
-// searchAll += JSON.stringify(recipes[0].description);
-// searchAll += JSON.stringify(recipes[0].appliance);
-// searchAll += JSON.stringify(recipes[0].ustensils);
-// console.log(searchAll);
-// recipes.forEach(e => searchAll.push(e.ingredients.forEach(i => i.ingredient)+e.description+e.appliance+e.ustensils));
-
-
-// recipes.forEach(recipe => console.log(recipe));
-// recipes.forEach(recipe => console.log(recipe.name));
-// recipes.forEach(recipe => console.log(recipe.ingredients));
-// recipes.forEach(recipe => console.log(recipe.description));
-// recipes.forEach(recipe => console.log(recipe.appliance));
-// recipes.forEach(recipe => console.log(recipe.ustensils));
-
-
-// let result = recipes[0].description.includes(searchText);
- // const result = recupIngredients[0].filter(
- //     (e) => e.ingredient == searchText       // tri sur ingredients
- // );
-
- // let idx = arrayAppliance.indexOf(searchText);
- // console.log(idx);
- // while (idx != -1) {
-//   indices.push(idx);
-//   idx = arrayAppliance.indexOf(searchText, idx + 1);
-// }
-        
-// function liste() {
-//     let liste = "";
-//     for(var i = 0; i < recipes.length; i++){
-//         liste += recipes.ingredients[i].ingredient+"\n";
-//         console.log(liste);
-//     }
-//     return liste;
-// };
-
-
-
-// // Ingredients
-// const recupIngredients=recipes.map(recipe => recipe.ingredients); // ingrédients de chaque recette'
-// console.log(recupIngredients);
-// let listIngredients=[];
-// recupIngredients.flat(1).forEach(ingredients => {
-//     if (!listIngredients.includes(ingredients.ingredient)) { // eviter les doublons
-//         listIngredients.push(ingredients.ingredient)}
-// });
-// // liste des ingredients triés
-// listIngredients.sort((a,b)=>a.localeCompare(b));
-// console.log(listIngredients);
-
-
-// // Appliance(appareils)
-// let listAppliance=[]; //liste des appliances
-// recipes.forEach(recipe => {
-//     if (!listAppliance.includes(recipe.appliance)) { // eviter les doublons
-//         listAppliance.push(recipe.appliance)}
-// });
-// // liste des appareils triés
-// listAppliance.sort((a,b)=>a.localeCompare(b))
-// console.log(listAppliance);
-
-
-// // Ustensils
-// let arrayUstensils=recipes.map(recipe => recipe.ustensils);
-// console.log(arrayUstensils);
-// // liste d'ustensils
-// let listUstensils=[];
-// arrayUstensils.flat().forEach(ustensil => {
-//     if (!listUstensils.includes(ustensil)) { // eviter les doublons
-//         listUstensils.push(ustensil)}
-// });
-// // liste des Ustensils triés
-// listUstensils.sort((a,b)=>a.localeCompare(b));
-// console.log(listUstensils);
-
-// console.log(indices);
-// fiches();
-// for (let i = 1; i <= recipes.length; i++) {
-//     document.getElementById(i).style.display = 'none';
-// }
-// recipes.forEach(id=> document.getElementById(id.id).style.display = 'none')
-// indices.forEach(id=> document.getElementById(id).style.display = '')
